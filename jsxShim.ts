44d9399b600e33dfbf13c8be98a6a5fe9d408f8d
Gabriele Petronella <gabriele@buildo.io>
2022-01-13 22:55:45 +0100

    Add build

---
+++ jsxShim.ts
@@ -0,0 +1,3 @@
+// See https://esbuild.github.io/content-types/#auto-import-for-jsx
+import * as React from "react";
+export { React };
---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "lib",
+  esbuildPlugins: [vanillaExtractPlugin()],
+  dts: true,
+  // See https://esbuild.github.io/content-types/#auto-import-for-jsx
+  inject: ["./jsxShim.ts"],
+});
