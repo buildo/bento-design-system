8a15b4079b4f31e76b42fa5e9af83d6479b1027a
Gabriele Petronella <gabriele@buildo.io>
2022-01-13 15:25:54 +0100

    Move types to createBentoBox

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,20 +1,4 @@
-import { ClassValue } from "clsx";
 import { baseSprinkles } from "../sprinkles.css";
 import { createBentoBox } from "./createBentoBox";
 
-type HTMLProperties = Omit<
-  React.AllHTMLAttributes<HTMLElement>,
-  "as" | "color" | "height" | "width" | "className"
->;
-
-export type BoxProps<AtomsFn extends typeof baseSprinkles> = {
-  as?: React.ElementType;
-  className?: ClassValue;
-  atoms?: Parameters<AtomsFn>[0];
-} & HTMLProperties;
-
 export const Box = createBentoBox(baseSprinkles);
-
-export type BoxType<AtomsFn extends typeof baseSprinkles> = React.ForwardRefExoticComponent<
-  BoxProps<AtomsFn> & React.RefAttributes<HTMLElement>
->;
