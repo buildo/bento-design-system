2fd32bdc99975e8505e189791b5966fe975a3886
Gabriele Petronella <gabriele@buildo.io>
2022-01-14 11:48:52 +0100

    Add note

--- src/internal/index.ts
+++ src/internal/index.ts
@@ -1,3 +1,5 @@
+// NOTE(gabro): don't import from ".." in this file, or it will cause a circular dependency
+
 import { createLayoutComponents } from "../Layout/createLayoutComponents";
 import { bentoSprinkles } from "./sprinkles.css";
 
