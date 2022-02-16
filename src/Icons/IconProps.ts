0969c481682c5446bd7479f0d46ac58e463a8f8b
Nemobot <nemobot@buildo.io>
2022-02-16 10:52:11 +0100

    Update Icon

--- src/Icons/IconProps.ts
+++ src/Icons/IconProps.ts
@@ -1,7 +1,7 @@
 import { vars } from "../vars.css";
 
 export type IconProps = {
-  size: Extract<keyof typeof vars.space, 8 | 16 | 24>;
+  size: Extract<keyof typeof vars.space, 8 | 12 | 16 | 24>;
   color?: "default" | "primary" | "informative" | "positive" | "warning" | "negative" | "disabled";
   className?: string;
 };
