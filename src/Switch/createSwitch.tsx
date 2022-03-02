de969e2a6aa1a5e88f2dab7525e649e0c59ab4c9
Federico Ercoles <federico.ercoles@buildo.io>
2022-03-02 11:55:51 +0100

    Implement Switch

---
+++ src/Switch/createSwitch.tsx
@@ -0,0 +1,95 @@
+import { useSwitch } from "@react-aria/switch";
+import { useFocusRing } from "@react-aria/focus";
+import { useField } from "@react-aria/label";
+import { mergeProps } from "@react-aria/utils";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
+import { useToggleState } from "@react-stately/toggle";
+import { useRef } from "react";
+import { FieldType } from "../Field/createField";
+import { Body, TextChildren } from "..";
+import { Box, Column, Columns } from "../internal";
+import { FieldProps } from "../Field/FieldProps";
+import { switchContainer, switchOuterRecipe, switchInnerRecipe } from "./Switch.css";
+import { SelectionControlConfig } from "../Field/SelectionControlConfig";
+
+export type SwitchProps = Omit<FieldProps<boolean>, "assistiveText"> & {
+  label: TextChildren;
+};
+
+type SwitchConfig = {
+  controlLabelSpacing: SelectionControlConfig["controlLabelSpacing"];
+};
+
+export function createSwitch(
+  Field: FieldType,
+  config: SwitchConfig = {
+    controlLabelSpacing: 8,
+  }
+) {
+  return function Switch(props: SwitchProps) {
+    const switchProps = {
+      ...props,
+      value: undefined,
+      isSelected: props.value,
+      isDisabled: props.disabled,
+      children: props.label,
+    };
+    const state = useToggleState(switchProps);
+    const ref = useRef<HTMLInputElement>(null);
+    const { inputProps } = useSwitch(switchProps, state, ref);
+    const { fieldProps, labelProps } = useField(switchProps);
+    const { isFocusVisible, focusProps } = useFocusRing();
+
+    return (
+      <Field {...props} label={undefined} assistiveTextProps={{}} errorMessageProps={{}}>
+        <Box
+          as="label"
+          disabled={props.disabled}
+          {...labelProps}
+          {...focusProps}
+          color={undefined}
+          cursor={{ default: "pointer", disabled: "notAllowed" }}
+          className={switchContainer}
+        >
+          <VisuallyHidden>
+            <input {...mergeProps(inputProps, fieldProps, focusProps)} ref={ref} />
+          </VisuallyHidden>
+          <Columns space={config.controlLabelSpacing} alignY="center">
+            <Column width="content">
+              <SwitchIndicator
+                value={props.value}
+                isFocusVisible={isFocusVisible}
+                isDisabled={props.disabled ?? false}
+              />
+            </Column>
+            <Body size="medium" color={props.disabled ? "disabled" : "default"}>
+              {props.label}
+            </Body>
+          </Columns>
+        </Box>
+      </Field>
+    );
+  };
+}
+
+type SwitchIndicatorProps = {
+  value: boolean;
+  isDisabled: boolean;
+  isFocusVisible: boolean;
+};
+
+function SwitchIndicator({ value, isFocusVisible, isDisabled }: SwitchIndicatorProps) {
+  return (
+    <Box
+      className={switchOuterRecipe({
+        isSelected: value,
+        isFocused: isFocusVisible,
+        isDisabled,
+      })}
+    >
+      <Box
+        className={switchInnerRecipe({ isSelected: value, isFocused: isFocusVisible, isDisabled })}
+      />
+    </Box>
+  );
+}
