7429c6c3c2eeb846b2adb67b05cabcf818ab0565
Gabriele Petronella <gabriele@buildo.io>
2022-03-08 09:59:03 +0100

    Use default size from config for ButtonLink

--- src/Button/ButtonLink.tsx
+++ src/Button/ButtonLink.tsx
@@ -21,7 +21,7 @@ export function createButtonLink(config: ButtonConfig = defaultButtonConfig) {
     href,
     kind,
     hierarchy,
-    size = "medium",
+    size = config.defaultSize,
     isDisabled,
     label,
     active = false,
