423849eed6b4532a5d1366cf14d119430b933a2a
veej <vincenzo@buildo.io>
2022-03-15 10:48:14 +0100

    fix Icon size prop

--- packages/bento-design-system/src/Icons/IconProps.ts
+++ packages/bento-design-system/src/Icons/IconProps.ts
@@ -1,7 +1,7 @@
-import { vars } from "../vars.css";
+import { BentoSprinkles } from "../internal";
 
 export type IconProps = {
-  size: Extract<keyof typeof vars.space, 8 | 16 | 24> | 12;
+  size: Extract<BentoSprinkles["width"], 8 | 16 | 24> | 12;
   color?:
     | "default"
     | "primary"
