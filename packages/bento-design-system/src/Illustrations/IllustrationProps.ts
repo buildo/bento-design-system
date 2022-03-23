61aba27803a30a7d73f4bb8a3a56916bb2237c85
Federico Ercoles <federico.ercoles@buildo.io>
2022-03-23 11:37:43 +0100

    Allow any number as size for illustrations

--- packages/bento-design-system/src/Illustrations/IllustrationProps.ts
+++ packages/bento-design-system/src/Illustrations/IllustrationProps.ts
@@ -1,5 +1,5 @@
 export type IllustrationProps = {
-  size: 24 | 32 | 40 | 80 | 160;
+  size: 24 | 32 | 40 | 80 | 160 | number;
 } & (
   | {
       style: "color";
--- packages/bento-design-system/src/Illustrations/svgIllustrationProps.ts
+++ packages/bento-design-system/src/Illustrations/svgIllustrationProps.ts
@@ -22,16 +22,5 @@ function outlineColor(color: (IllustrationProps & { style: "outline" })["color"]
 }
 
 function sizeToDimensions(size: IllustrationProps["size"]): { width: number; height: number } {
-  switch (size) {
-    case 24:
-      return { width: 24, height: 24 };
-    case 32:
-      return { width: 32, height: 32 };
-    case 40:
-      return { width: 40, height: 40 };
-    case 80:
-      return { width: 80, height: 80 };
-    case 160:
-      return { width: 160, height: 160 };
-  }
+  return { width: size, height: size };
 }
