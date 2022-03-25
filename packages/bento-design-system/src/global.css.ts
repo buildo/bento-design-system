8810b85b8f541cec7b69c3aa43692a34c53b1baa
Gabriele Petronella <gabriele@buildo.io>
2022-03-25 16:57:01 +0100

    Introduce isolation: isolate on #root element

---
+++ packages/bento-design-system/src/global.css.ts
@@ -0,0 +1,9 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("#root", {
+  // NOTE(gabro): we make sure the root element introduces a stacking context, so that portals
+  // (that get rendered below the #root element) are always rendered in front of it
+  // (e.g.modals, tooltips etc).
+  // This assumes the root element is identified by #root, which is the case for regular CRA apps.
+  isolation: "isolate",
+});
