501f01feedbca8a66fb97a63797ee18a15247a55
Vincenzo Guerrisi <vincenzo@buildo.io>
2022-04-04 12:35:48 +0200

    implement Tiles

---
+++ packages/bento-design-system/src/Layout/createTiles.tsx
@@ -0,0 +1,32 @@
+import flattenChildren from "react-keyed-flatten-children";
+import { Children } from "..";
+import { mobileColumns, tabletColumns, desktopColumns } from "./Tiles.css";
+import { ResponsiveAlignY, alignYToFlexAlign } from "../util/align";
+import { bentoSprinkles } from "../internal";
+import { BoxProps, BoxType } from "../Box/createBentoBox";
+import { normalizeResponsiveValue, RequiredResponsiveValue } from "../internal/sprinkles.css";
+
+export function createTiles<AtomsFn extends typeof bentoSprinkles>(Box: BoxType<AtomsFn>) {
+  type ResponsiveSpace = BoxProps<AtomsFn>["gap"];
+
+  type Props = {
+    space: ResponsiveSpace;
+    columns: RequiredResponsiveValue<1 | 2 | 3 | 4 | 5 | 6>;
+    children: Children;
+    alignY?: ResponsiveAlignY;
+  };
+  return function Tiles({ space, columns, alignY, children }: Props) {
+    const { desktop, tablet, mobile } = normalizeResponsiveValue(columns);
+    const className = [
+      desktop && desktopColumns[desktop],
+      tablet && tabletColumns[tablet],
+      mobile && mobileColumns[mobile],
+    ];
+
+    return (
+      <Box className={className} display="grid" gap={space} alignItems={alignYToFlexAlign(alignY)}>
+        {flattenChildren(children)}
+      </Box>
+    );
+  };
+}
---
+++ packages/storybook/stories/Layout/Tiles.stories.tsx
@@ -0,0 +1,51 @@
+import { Placeholder, Tiles } from "..";
+import {
+  alignYArgType,
+  createComponentStories,
+  disableControlArgType,
+  spaceArgType,
+} from "../util";
+
+const { defaultExport, createStory } = createComponentStories({
+  component: Tiles,
+  args: {},
+  argTypes: {
+    space: spaceArgType,
+    columns: {
+      options: [1, 2, 3, 4, 5, 6],
+    },
+    children: disableControlArgType,
+    alignY: alignYArgType,
+  },
+});
+
+export default defaultExport;
+
+export const threeColumns = createStory({
+  space: 32,
+  columns: 3,
+  children: [<Placeholder key={1} />, <Placeholder key={2} />, <Placeholder key={3} />],
+});
+
+export const responsive = createStory({
+  space: { mobile: 8, tablet: 16, desktop: 32 },
+  columns: { mobile: 1, tablet: 2, desktop: 4 },
+  children: [
+    <Placeholder key={1} />,
+    <Placeholder key={2} />,
+    <Placeholder key={3} />,
+    <Placeholder key={4} />,
+  ],
+});
+
+export const alignY = createStory({
+  columns: 4,
+  space: 32,
+  alignY: "bottom",
+  children: [
+    <Placeholder key={1} />,
+    <Placeholder height={400} key={2} />,
+    <Placeholder key={3} />,
+    <Placeholder key={4} />,
+  ],
+});
