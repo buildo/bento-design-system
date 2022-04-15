8744161cbdc750c007e05ffceb5b09454c9f75f1
Vincenzo Guerrisi <vincenzo@buildo.io>
2022-04-15 18:35:15 +0200

    Fix InlineLoader

--- packages/bento-design-system/src/Icons/IconSpinner.tsx
+++ packages/bento-design-system/src/Icons/IconSpinner.tsx
@@ -4,7 +4,7 @@ import { svgIconProps } from "./svgIconProps";
 export function IconSpinner(props: IconProps) {
   return (
     <svg {...svgIconProps(props)}>
-      <path d="M8 1.5A6.5 6.5 0 0 0 1.5 8 .75.75 0 1 1 0 8a8 8 0 1 1 8 8 .75.75 0 1 1 0-1.5 6.5 6.5 0 1 0 0-13Z" />
+      <path d="M12 2.25A9.75 9.75 0 0 0 2.25 12 1.125 1.125 0 1 1 0 12a12 12 0 1 1 12 12 1.125 1.125 0 1 1 0-2.25 9.75 9.75 0 1 0 0-19.5Z" />
     </svg>
   );
 }
