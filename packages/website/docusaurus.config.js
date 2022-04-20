03edf73f89c72976eed9751e33c38e8708709b75
Gabriele Petronella <gabriele@buildo.io>
2022-04-20 15:35:50 +0200

    Fix build

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -75,16 +75,8 @@ const config = {
             title: "Community",
             items: [
               {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
                 label: "Twitter",
-                href: "https://twitter.com/docusaurus",
+                href: "https://twitter.com/buildoHQ",
               },
             ],
           },
@@ -92,12 +84,8 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/buildo/bento-design-system",
               },
             ],
           },
