fac6c067e37591e16061c3677c0fec3b50b94a8c
Vincenzo Guerrisi <vincenzo@buildo.io>
2022-04-22 18:01:46 +0200

    docs introduction

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -22,6 +22,10 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/buildo/bento-design-system/tree/main/docs/",
+          remarkPlugins: [[require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }]],
+        },
+        pages: {
+          remarkPlugins: [require("@docusaurus/remark-plugin-npm2yarn")],
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
