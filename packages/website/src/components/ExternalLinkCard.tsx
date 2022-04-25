5326914e97c8543c21bca9291357549ba443c4d2
Gabriele Petronella <gabriele@buildo.io>
2022-04-25 17:25:12 +0200

    Add external links to ComponentDoc

---
+++ packages/website/src/components/ExternalLinkCard.tsx
@@ -0,0 +1,62 @@
+import config from "@generated/docusaurus.config";
+import * as React from "react";
+import styles from "./ExternalLinkCard.module.css";
+import { useColorMode } from "@docusaurus/theme-common";
+
+type Props = {
+  label: string;
+} & (
+  | {
+      type: "github";
+      componentName: string;
+    }
+  | {
+      type: "zeroheight";
+      slug: string;
+    }
+);
+
+export function ExternalLinkCard(props: Props) {
+  const { colorMode } = useColorMode();
+
+  const link = ((): string => {
+    switch (props.type) {
+      case "github":
+        return `https://github.com/${config.organizationName}/${config.projectName}/tree/main/packages/bento-design-system/src/${props.componentName}`;
+      case "zeroheight":
+        return `https://zeroheight.com/1424b583b/p/${props.slug}`;
+    }
+  })();
+
+  const image = (() => {
+    switch (props.type) {
+      case "github":
+        switch (colorMode) {
+          case "dark":
+            return "/img/github_dark.svg";
+          case "light":
+            return "/img/github_light.svg";
+        }
+      case "zeroheight":
+        return "/img/zeroheight.png";
+    }
+  })();
+
+  const label = (() => {
+    switch (props.type) {
+      case "github":
+        return "GitHub";
+      case "zeroheight":
+        return "zeroheight";
+    }
+  })();
+
+  return (
+    <a className={styles.card} href={link} target="_blank" rel="noreferrer">
+      <div className={styles.image}>
+        <img src={image} alt="" />
+      </div>
+      {label}
+    </a>
+  );
+}
