ec40ffe75f4b66816c503ec8edd3ff24e93396d3
Gabriele Petronella <gabriele@buildo.io>
2022-04-26 12:37:47 +0200

    Fix types

--- packages/website/src/components/Canvas.tsx
+++ packages/website/src/components/Canvas.tsx
@@ -1,5 +1,6 @@
 import CodeBlock from "@theme/CodeBlock";
 import * as React from "react";
+import { Expression } from "@babel/types";
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
 import generate from "@babel/generator";
@@ -7,11 +8,11 @@ import prettier from "prettier/standalone";
 import parserTypeScript from "prettier/parser-typescript";
 
 export function Canvas({ path }: { path: string }) {
-  let rawSource: string | null = null;
+  let rawSource = null as string | null;
   import(`!!raw-loader!@site/src/snippets/${path}`).then((s) => {
     rawSource = s;
   });
-  let Component: React.ComponentType | null = null;
+  let Component = null as React.ElementType | null;
   import(`@site/src/snippets/${path}`).then((C) => {
     Component = C;
   });
@@ -24,7 +25,7 @@ export function Canvas({ path }: { path: string }) {
     sourceType: "module",
     plugins: ["jsx", "typescript"],
   });
-  let componentBodyAst = null;
+  let componentBodyAst: Expression | null | undefined = null;
   traverse(ast, {
     enter(path) {
       if (path.node.type === "ReturnStatement") {
@@ -33,18 +34,21 @@ export function Canvas({ path }: { path: string }) {
     },
   });
 
-  const source = prettier.format(generate(componentBodyAst).code, {
-    parser: "typescript",
-    plugins: [parserTypeScript],
-    jsxSingleQuote: false,
-  });
+  let source: string | null = null;
+  if (componentBodyAst) {
+    source = prettier.format(generate(componentBodyAst).code, {
+      parser: "typescript",
+      plugins: [parserTypeScript],
+      jsxSingleQuote: false,
+    });
+  }
 
   return (
     <>
       <div style={{ padding: 16, border: "1px solid slategray" }}>
         <Component />
       </div>
-      <CodeBlock language="jsx">{source}</CodeBlock>
+      {source && <CodeBlock language="jsx">{source}</CodeBlock>}
     </>
   );
 }
