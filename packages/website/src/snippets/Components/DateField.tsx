42df022b91fc2c7d8f14336d20d279142806cd7e
Vincenzo Guerrisi <vincenzo@buildo.io>
2022-04-29 18:10:30 +0200

    fix doc snippet

--- packages/website/src/snippets/Components/DateField.tsx
+++ packages/website/src/snippets/Components/DateField.tsx
@@ -2,6 +2,15 @@ import * as React from "react";
 import { DateField } from "..";
 
 export default function ChipExample() {
-  const [value, setValue] = React.useState(new Date());
-  return <DateField label="Birth date" value={value} onChange={setValue} />;
+  const [value, setValue] = React.useState<Date | null>(new Date());
+  return (
+    <DateField
+      type="single"
+      name="birth-date"
+      label="Birth date"
+      value={value}
+      onChange={setValue}
+      onBlur={() => {}}
+    />
+  );
 }
