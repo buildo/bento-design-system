8a8f3b8d47cf4469076eb386851f439e084e4b7a
Vincenzo Guerrisi <vincenzo@buildo.io>
2022-05-03 16:04:37 +0200

    customize docusaurus theme

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
+          <Link className="button button--primary button--lg" to="/docs/intro">
             Get started
           </Link>
         </div>
