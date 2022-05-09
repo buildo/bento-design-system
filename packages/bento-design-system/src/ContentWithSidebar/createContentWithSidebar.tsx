aa782c9e93d4379216702afbbf071b062f7f5c8b
Gabriele Petronella <gabriele@buildo.io>
2022-05-09 12:56:24 +0200

    Add some docs

--- packages/bento-design-system/src/ContentWithSidebar/createContentWithSidebar.tsx
+++ packages/bento-design-system/src/ContentWithSidebar/createContentWithSidebar.tsx
@@ -9,17 +9,55 @@ import { Children } from "../util/Children";
 import { Column } from "../internal";
 import { ComponentProps } from "react";
 
+/**
+ * A component that renders a main content and a sidebar on the side.
+ *
+ * The main content is always rendered before the sidebar in the DOM, so that tab and focus order goes to the main content first.
+ */
 export function createContentWithSidebar<AtomsFn extends typeof bentoSprinkles>(
   Box: BoxType<AtomsFn>
 ) {
   type Props = {
+    /**
+     * Main content and sidebar to render. The order depends on the sidebar position:
+     * - "left" -> [sidebar, content]
+     * - "right" -> [content, sidebar]
+     */
     children: [Children, Children];
-    space?: BoxProps<AtomsFn>["gap"];
-    sidebarBackground?: BoxProps<AtomsFn>["background"];
-    sidebarWidth?: ComponentProps<typeof Column>["width"] | { custom: string | number };
-    sidebarAs?: BoxProps<AtomsFn>["as"];
-    contentAs?: BoxProps<AtomsFn>["as"];
+    /**
+     * The placement of the sidebar relative to the content. Note that this is purely visual and it does not affect the
+     * DOM order: the content always comes first in the DOM, so that it receives focus first when navigating with a
+     * keyboard or a screen reader.
+     */
     sidebarPosition: "right" | "left";
+    /**
+     * Width of the sidebar. It can be a shortcut width like with `<Columns>` (e.g. "1/5") or a completely custom width.
+     * Examples:
+     *
+     * ```tsx
+     *  <ContentWithSidebar sidebarPosition="right" sidebarWidth="1/5">...</ContentWithSidebar>
+     *  <ContentWithSidebar  sidebarPosition="right" sidebarWidth={{ custom: "25%" }}>...</ContentWithSidebar>
+     * ```
+     */
+    sidebarWidth: ComponentProps<typeof Column>["width"] | { custom: string | number };
+    /**
+     * Horizontal space between the content and the sidebar. Defaults to no content.
+     */
+    space?: BoxProps<AtomsFn>["gap"];
+    /**
+     * Background of the sidebar container, defaults to no background.
+     */
+    sidebarBackground?: BoxProps<AtomsFn>["background"];
+    /**
+     * Native dom element for the sidebar.
+     * @default <aside>
+     */
+    sidebarAs?: BoxProps<AtomsFn>["as"];
+    /**
+     * Native dom element for the content.
+     * @default <main>
+     */
+    contentAs?: BoxProps<AtomsFn>["as"];
   };
 
   return function ContentWithSidebar({
