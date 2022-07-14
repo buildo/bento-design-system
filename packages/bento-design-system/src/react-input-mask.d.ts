59a2737c4c64862d3d9ff965b8cbe56cf5124d60
Gabriele Petronella <gabriele@buildo.io>
2022-07-14 09:57:50 +0200

    Fix react-input-mask children type

---
+++ packages/bento-design-system/src/react-input-mask.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import "react-input-mask";
+
+declare module "react-input-mask" {
+  interface Props {
+    children: (inputProps: InputHTMLAttributes<HTMLInputElement>) => JSX.Element;
+  }
+}
