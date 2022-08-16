f96a97aa7e18cd23f6695ab7b273ba0e9fbe57c6
Gabriele Petronella <gabriele@buildo.io>
2022-08-16 15:34:27 +0000

    Rewrite Tooltip

--- packages/bento-design-system/src/Tooltip/Tooltip.tsx
+++ packages/bento-design-system/src/Tooltip/Tooltip.tsx
@@ -5,7 +5,7 @@ import { useTooltipTriggerState } from "@react-stately/tooltip";
 import { useTooltipTrigger, useTooltip } from "@react-aria/tooltip";
 import { tooltip, arrow as arrowStyle } from "./Tooltip.css";
 import { createPortal } from "../util/createPortal";
-import { TooltipConfig } from "./Config";
+import { useBentoConfig } from "../BentoConfigProvider";
 
 type Props = {
   content: Children;
@@ -22,68 +22,67 @@ type Props = {
   ) => JSX.Element;
 };
 
-export function createTooltip(config: TooltipConfig) {
-  return function Tooltip(props: Props) {
-    const arrowRef = useRef<HTMLElement | null>(null);
-    const {
-      x,
-      y,
-      reference,
-      floating,
-      strategy,
-      placement,
-      middlewareData: { arrow: arrowData },
-    } = useFloating({
-      middleware: [shift(), autoPlacement(), offset(8), arrow({ element: arrowRef })],
-    });
-    const trigger = { delay: 500 };
-    const state = useTooltipTriggerState(trigger);
-    const ref = useRef<HTMLElement | null>(null);
-    const { triggerProps, tooltipProps: _tooltipProps } = useTooltipTrigger(trigger, state, ref);
-    const { tooltipProps } = useTooltip(_tooltipProps);
+export function Tooltip(props: Props) {
+  const config = useBentoConfig().tooltip;
+  const arrowRef = useRef<HTMLElement | null>(null);
+  const {
+    x,
+    y,
+    reference,
+    floating,
+    strategy,
+    placement,
+    middlewareData: { arrow: arrowData },
+  } = useFloating({
+    middleware: [shift(), autoPlacement(), offset(8), arrow({ element: arrowRef })],
+  });
+  const trigger = { delay: 500 };
+  const state = useTooltipTriggerState(trigger);
+  const ref = useRef<HTMLElement | null>(null);
+  const { triggerProps, tooltipProps: _tooltipProps } = useTooltipTrigger(trigger, state, ref);
+  const { tooltipProps } = useTooltip(_tooltipProps);
 
-    // See https://floating-ui.com/docs/tutorial#arrow-middleware
-    const position: string = placement.split("-")[0];
-    const staticSide = {
-      top: "bottom",
-      right: "left",
-      bottom: "top",
-      left: "right",
-    }[position]!;
+  // See https://floating-ui.com/docs/tutorial#arrow-middleware
+  const position: string = placement.split("-")[0];
+  const staticSide = {
+    top: "bottom",
+    right: "left",
+    bottom: "top",
+    left: "right",
+  }[position]!;
 
-    const arrowPosition = {
-      left: Number.isFinite(arrowData?.x) ? arrowData?.x : undefined,
-      top: Number.isFinite(arrowData?.y) ? arrowData?.y : undefined,
-      [staticSide]: -4,
-    };
-
-    return (
-      <>
-        {props.trigger(reference, triggerProps)}
-        {state.isOpen &&
-          createPortal(
-            <Box
-              className={tooltip}
-              borderRadius={config.radius}
-              ref={floating}
-              style={{
-                position: strategy,
-                top: y ?? "",
-                left: x ?? "",
-              }}
-              {...tooltipProps}
-            >
-              <Inset spaceX={config.paddingX} spaceY={config.paddingY}>
-                <Label size={config.labelSize} color="primaryInverse">
-                  {props.content}
-                </Label>
-              </Inset>
-              <Box ref={arrowRef} className={arrowStyle} style={arrowPosition} />
-            </Box>
-          )}
-      </>
-    );
+  const arrowPosition = {
+    left: Number.isFinite(arrowData?.x) ? arrowData?.x : undefined,
+    top: Number.isFinite(arrowData?.y) ? arrowData?.y : undefined,
+    [staticSide]: -4,
   };
+
+  return (
+    <>
+      {props.trigger(reference, triggerProps)}
+      {state.isOpen &&
+        createPortal(
+          <Box
+            className={tooltip}
+            borderRadius={config.radius}
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? "",
+              left: x ?? "",
+            }}
+            {...tooltipProps}
+          >
+            <Inset spaceX={config.paddingX} spaceY={config.paddingY}>
+              <Label size={config.labelSize} color="primaryInverse">
+                {props.content}
+              </Label>
+            </Inset>
+            <Box ref={arrowRef} className={arrowStyle} style={arrowPosition} />
+          </Box>
+        )}
+    </>
+  );
 }
 
 export type { Props as TooltipProps };
