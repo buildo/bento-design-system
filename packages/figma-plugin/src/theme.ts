1d9d16b9137e72a931931c9f039076fb8fe5d0f7
marcopiii <marpi93@icloud.com>
2022-09-22 11:23:43 +0200

    extract Data Visualization and Elevation

--- packages/figma-plugin/src/theme.ts
+++ packages/figma-plugin/src/theme.ts
@@ -51,6 +51,112 @@ function theme() {
       textNegative: getStyleColor("Text/Negative"),
       textDisabled: getStyleColor("Text/Disabled"),
     },
+    interactiveBackgroundColor: {
+      // primarySolidEnabledBackground:
+      // primarySolidHoverBackground:
+      // primarySolidFocusBackground:
+      // primaryTransparentEnabledBackground:
+      // primaryTransparentHoverBackground:
+      // primaryTransparentFocusBackground:
+      // secondarySolidEnabledBackground:
+      // secondarySolidHoverBackground:
+      // secondarySolidFocusBackground:
+      // secondaryTransparentEnabledBackground:
+      // secondaryTransparentHoverBackground:
+      // secondaryTransparentFocusBackground:
+      // dangerSolidEnabledBackground:
+      // dangerSolidHoverBackground:
+      // dangerSolidFocusBackground:
+      // dangerTransparentEnabledBackground:
+      // dangerTransparentHoverBackground:
+      // dangerTransparentFocusBackground:
+      // disabledSolidBackground:
+      // disabledTransparentBackground:
+    },
+    interactiveForegroundColor: {
+      // primarySolidEnabledForeground:
+      // primarySolidHoverForeground:
+      // primarySolidFocusForeground:
+      // primaryTransparentEnabledForeground:
+      // primaryTransparentHoverForeground:
+      // primaryTransparentFocusForeground:
+      // secondarySolidEnabledForeground:
+      // secondarySolidHoverForeground:
+      // secondarySolidFocusForeground:
+      // secondaryTransparentEnabledForeground:
+      // secondaryTransparentHoverForeground:
+      // secondaryTransparentFocusForeground:
+      // dangerSolidEnabledForeground:
+      // dangerSolidHoverForeground:
+      // dangerSolidFocusForeground:
+      // dangerTransparentEnabledForeground:
+      // dangerTransparentHoverForeground:
+      // dangerTransparentFocusForeground:
+      // disabledSolidForeground:
+      // disabledTransparentForeground:
+      // linkEnabled:
+      // linkHover:
+      // linkFocus:
+      // linkDisabled:
+      // linkEnabledInverse:
+      // linkHoverInverse:
+      // linkFocusInverse:
+      // linkDisabledInverse:
+    },
+    outlineColor: {
+      outlineInteractive: getStyleColor("Outline/Interactive"),
+      outlineDecorative: getStyleColor("Outline/Decorative"),
+      outlineContainer: getStyleColor("Outline/Container"),
+      outlineInputEnabled: getStyleColor("Outline/Input Enabled"),
+      outlineInputHover: getStyleColor("Outline/Input Hover"),
+      outlineInputFocus: getStyleColor("Outline/Input Focus"),
+      outlineInputDisabled: getStyleColor("Outline/Input Disabled"),
+      outlineInformative: getStyleColor("Outline/Informative"),
+      outlinePositive: getStyleColor("Outline/Positive"),
+      outlineWarning: getStyleColor("Outline/Warning"),
+      outlineNegative: getStyleColor("Outline/Negative"),
+    },
+    dataVisualizationColor: {
+      softGrey: getStyleColor("Data Visualization/Soft Grey"),
+      softRed: getStyleColor("Data Visualization/Soft Red"),
+      softOrange: getStyleColor("Data Visualization/Soft Orange"),
+      softYellow: getStyleColor("Data Visualization/Soft Yellow"),
+      softGreen: getStyleColor("Data Visualization/Soft Green"),
+      softJade: getStyleColor("Data Visualization/Soft Jade"),
+      softBlue: getStyleColor("Data Visualization/Soft Blue"),
+      softIndigo: getStyleColor("Data Visualization/Soft Indigo"),
+      softViolet: getStyleColor("Data Visualization/Soft Violet"),
+      softPink: getStyleColor("Data Visualization/Soft Pink"),
+      brightGrey: getStyleColor("Data Visualization/Bright Grey"),
+      brightRed: getStyleColor("Data Visualization/Bright Red"),
+      brightOrange: getStyleColor("Data Visualization/Bright Orange"),
+      brightYellow: getStyleColor("Data Visualization/Bright Yellow"),
+      brightGreen: getStyleColor("Data Visualization/Bright Green"),
+      brightJade: getStyleColor("Data Visualization/Bright Jade"),
+      brightBlue: getStyleColor("Data Visualization/Bright Blue"),
+      brightIndigo: getStyleColor("Data Visualization/Bright Indigo"),
+      brightViolet: getStyleColor("Data Visualization/Bright Violet"),
+      brightPink: getStyleColor("Data Visualization/Bright Pink"),
+    },
+    boxShadow: {
+      // outlineInteractive:
+      // outlineInteractiveBottom:
+      // outlineDecorative:
+      // outlineDecorativeBottom:
+      // outlineContainer:
+      // outlineInputEnabled:
+      // outlineInputHover:
+      // outlineInputFocus:
+      // outlineInputDisabled:
+      // outlineInformative:
+      // outlinePositive:
+      // outlineWarning:
+      // outlineNegative:
+      // outlineNegativeStrong:
+      elevationSmall: getEffectStyle("Elevation/Small"),
+      elevationMedium: getEffectStyle("Elevation/Medium"),
+      elevationLarge: getEffectStyle("Elevation/Large"),
+    },
   };
 }
 
@@ -69,4 +175,33 @@ function getStyleColor(name: string): string | undefined {
     default:
       console.warn("Unhandled paint type", paint.type);
   }
+}
+
+function getEffectStyle(name: string): string | undefined {
+  const styles = figma.getLocalEffectStyles();
+  const effect = styles.find((s) => s.name === name)?.effects[0];
+
+  if (!effect) {
+    console.warn(`No effect found for name '${name}'`);
+    return;
+  }
+
+  switch (effect.type) {
+    case "DROP_SHADOW":
+    case "INNER_SHADOW":
+      return figmaEffectToBoxShadow(effect);
+    default:
+      console.warn("Unhandled effect type", effect.type);
+  }
+}
+
+function figmaEffectToBoxShadow(effect: Extract<Effect, { type: "DROP_SHADOW" | "INNER_SHADOW" }>) {
+  const offset = `${effect.offset.x}px ${effect.offset.y}px`;
+  const radius = `${effect.radius}px`;
+  const spread = `${effect.spread ?? 0}px`;
+  const color = figmaRGBToHex(effect.color);
+
+  const prefix = effect.type === "INNER_SHADOW" ? "inset " : "";
+
+  return prefix + [offset, radius, spread, color].join(" ");
 }
