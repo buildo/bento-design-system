0108528636dbd467d64c7b8f1965c6f532fe9071
Gabriele Petronella <gabriele@buildo.io>
2022-10-06 11:34:27 +0200

    Fix bug when nesting BentoConfigProvider
    Previously we would merge the partial config given to each provider with
    `defaultConfig`, which would effectively "reset" every other config in the
    descendent component tree.
    
    Now we instead merge each provider's partial config with their parent
    config, which is `defaultConfig` for the top-level one.

---
+++ packages/storybook/stories/Utils/withBentoConfig.stories.tsx
@@ -0,0 +1,49 @@
+import {
+  DesignSystemProvider,
+  Modal as BentoModal,
+  withBentoConfig,
+} from "@buildo/bento-design-system";
+import { action } from "@storybook/addon-actions";
+import { defaultMessages } from "../defaultMessages";
+
+export default {};
+
+// This tests that `withBentoConfig` only overrides the specific config passed to it.
+// In this case we check that when we customize the Modal, we don't change the
+// Button configuration that was provided by the top-level DesignSystemProvider.
+//
+// In practice, we should see a custom Modal with its radius and padding overridden,
+// and also a custom Button with its radius overridden by the top-level config.
+export const ConfiguredModal = () => {
+  const Modal = withBentoConfig(
+    {
+      modal: {
+        radius: 16,
+        paddingX: 80,
+      },
+    },
+    BentoModal
+  );
+
+  return (
+    <DesignSystemProvider
+      defaultMessages={defaultMessages}
+      config={{
+        button: {
+          radius: 16,
+        },
+      }}
+    >
+      <Modal
+        title="Custom Modal"
+        onClose={action("onClose")}
+        primaryAction={{
+          label: "Primary action",
+          onPress: action("onPrimaryActionPress"),
+        }}
+      >
+        Modal content
+      </Modal>
+    </DesignSystemProvider>
+  );
+};
