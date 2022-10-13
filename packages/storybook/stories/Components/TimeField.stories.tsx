caa72db370f6945e3b4e1d18289b680e63315de0
Gabriele Petronella <gabriele@buildo.io>
2022-10-13 15:07:59 +0200

    Add and explain workaround for Readonly story

--- packages/storybook/stories/Components/TimeField.stories.tsx
+++ packages/storybook/stories/Components/TimeField.stories.tsx
@@ -23,11 +23,19 @@ export const Error = createControlledStory("", {
   issues: ["This time is not available"],
 });
 
-const x = new Time(12, 30);
-
-console.log(x);
-
-// export const ReadOnly = createStory({
-//   value: x,
-//   isReadOnly: true,
-// });
+export const ReadOnly = createStory({
+  // NOTE(gabro): ok this is a weird bug of Storybook
+  // It should use `new Time(10, 15)` but it generates a bug when doing so.
+  // The reason seems to be that the constructor is minified
+  // in @internationalized/date to a string starting with $ followed by digits.
+  // Apparently the Storybook "reviver" function (which I assume restores the
+  // manager cache accidentally strips the $ from the string, resulting in an
+  // invalid constructor name (starting with a digit).
+  // In practice, this is a Storybook-specific bug, so we'll just work around it
+  // and hope that future versions of Storybook will fix it.
+  value: {
+    hour: 10,
+    minute: 15,
+  } as Time,
+  isReadOnly: true,
+});
