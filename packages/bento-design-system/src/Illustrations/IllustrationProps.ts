de9ab73c45f6c7abe2211d0d07bf1d3ac784d04c
Gabriele Petronella <gabriele@buildo.io>
2022-10-14 12:31:00 +0200

    Deprecate IllustrationProps "style" in favor of "kind"

--- packages/bento-design-system/src/Illustrations/IllustrationProps.ts
+++ packages/bento-design-system/src/Illustrations/IllustrationProps.ts
@@ -1,11 +1,22 @@
+// NOTE(gabro): we deprecated the `style` prop due to false positive warnings in
+// popular ESLint configs, which expect `style` to be an object.
+// We're still keeping the old props around for backwards compatibility.
 export type IllustrationProps = {
   size: 24 | 32 | 40 | 80 | 160 | { custom: number };
 } & (
   | {
-      style: "color";
+      kind: "color";
+      /**
+       * @deprecated use `kind` instead
+       */
+      style?: "color";
     }
   | {
-      style: "outline";
+      kind: "outline";
+      /**
+       * @deprecated use `kind` instead
+       */
+      style?: "outline";
       color: "default" | "disabled" | "inherit";
     }
 );
