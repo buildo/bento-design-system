c126849258ae997734c3987ac722fa41e39367d0
Gabriele Petronella <gabriele@buildo.io>
2022-10-14 12:37:32 +0200

    Improve types

--- packages/bento-design-system/src/Illustrations/IllustrationProps.ts
+++ packages/bento-design-system/src/Illustrations/IllustrationProps.ts
@@ -1,22 +1,29 @@
+import { AtLeast } from "../util/AtLeast";
+
 // NOTE(gabro): we deprecated the `style` prop due to false positive warnings in
 // popular ESLint configs, which expect `style` to be an object.
 // We're still keeping the old props around for backwards compatibility.
 export type IllustrationProps = {
   size: 24 | 32 | 40 | 80 | 160 | { custom: number };
 } & (
-  | {
-      kind: "color";
-      /**
-       * @deprecated use `kind` instead
-       */
-      style?: "color";
-    }
-  | {
-      kind: "outline";
-      /**
-       * @deprecated use `kind` instead
-       */
-      style?: "outline";
-      color: "default" | "disabled" | "inherit";
-    }
+  | AtLeast<
+      {
+        kind: "color";
+        /**
+         * @deprecated use `kind` instead
+         */
+        style: "color";
+      },
+      "kind" | "style"
+    >
+  | (AtLeast<
+      {
+        kind: "outline";
+        /**
+         * @deprecated use `kind` instead
+         */
+        style: "outline";
+      },
+      "kind" | "style"
+    > & { color: "default" | "disabled" | "inherit" })
 );
