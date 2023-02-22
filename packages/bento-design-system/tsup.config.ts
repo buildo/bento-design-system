7a142764b4561d60f2620ab65d6d9a051a003d10
Gabriele Petronella <gabriele@buildo.io>
2023-02-22 15:14:57 +0100

    Export default defaultMessages for the English language

--- packages/bento-design-system/tsup.config.ts
+++ packages/bento-design-system/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/defaultTheme.ts"],
+  entry: ["src/index.ts", "src/defaultTheme.ts", "src/defaultMessages/en.ts"],
   sourcemap: true,
   outDir: "lib",
   format: ["esm", "cjs"],
