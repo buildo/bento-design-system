1fa3259f8347d5d27d652cc227ab4b5bcdc516d1
Vincenzo Guerrisi <vincenzo@buildo.io>
2023-05-17 12:03:07 +0200

    fix custom icon story

--- packages/storybook/stories/Components/Feedback.stories.tsx
+++ packages/storybook/stories/Components/Feedback.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from "@storybook/addon-actions";
-import { Feedback, IllustrationIdea } from "..";
+import { Feedback, IconLightbulb, IllustrationIdea } from "..";
 import { createComponentStories, textArgType } from "../util";
 
 const { defaultExport, createStory } = createComponentStories({
@@ -50,8 +50,9 @@ export const WrappingTitleAndDescription = createStory({
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 });
 
-export const CustomIllustration = createStory({
-  illustration: IllustrationIdea,
+export const CustomIcon = createStory({
+  icon: IconLightbulb,
+  status: undefined,
   description: "Description",
   action: {
     onPress: action("On click"),
