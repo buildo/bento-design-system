e51ddec5271ae1efd014c52c87106fadd23b56d3
Gabriele Petronella <gabriele@buildo.io>
2023-06-21 16:22:15 +0200

    Add documentation about 'as' prop

---
+++ packages/website/src/snippets/Typography/TitleH2.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Title } from "..";
+
+export default function TitleExample() {
+  return <Title as="h2" size="large">{`A title which is rendered as <h2>`}</Title>;
+}
