d7bdfda567dec743e9da0d9433c049fc06c7a059
Federico Ercoles <federico.ercoles@buildo.io>
2023-06-26 12:32:39 +0200

    Fix Pagination story

--- packages/storybook/stories/Components/Pagination.stories.tsx
+++ packages/storybook/stories/Components/Pagination.stories.tsx
@@ -1,35 +1,33 @@
-import { StoryObj } from "@storybook/react";
-import { PaginationItemsPerPage as ItemsPerPage, Pagination, PaginationProps } from "../";
+import { Meta, StoryObj } from "@storybook/react";
+import { PaginationItemsPerPage as ItemsPerPage, Pagination } from "../";
 import { action } from "@storybook/addon-actions";
-
-const props: PaginationProps = {
-  page: 1,
-  pageCount: 5,
-  itemsPerPage: 5,
-  itemsPerPageOptions: {
-    5: "5",
-    10: "10",
-    20: "20",
-    50: "50",
-  },
-  onPageChange: (page: number) => action(`Page changed to ${page}`),
-  onItemsPerPageChange: (itemsPerPage: ItemsPerPage[number]) =>
-    action(`Items per page changed to ${itemsPerPage}`),
-  messages: {
-    itemsPerPageOptionsLabel: "items per page",
-    currentPageItemsLabel: (start: number, end: number, total: number) =>
-      `${start}-${end} of ${total} items`,
-    pageCountLabel: (pageCount: number) => `of ${pageCount} pages`,
-    singlePageLabel: "1 page",
-    previousPageButtonLabel: "Previous page",
-    nextPageButtonLabel: "Next page",
-  },
-};
 
 const meta = {
   component: Pagination,
-  args: props,
-};
+  args: {
+    page: 1,
+    pageCount: 5,
+    itemsPerPage: 5,
+    itemsPerPageOptions: {
+      5: "5",
+      10: "10",
+      20: "20",
+      50: "50",
+    },
+    onPageChange: (page: number) => action(`Page changed to ${page}`),
+    onItemsPerPageChange: (itemsPerPage: ItemsPerPage[number]) =>
+      action(`Items per page changed to ${itemsPerPage}`),
+    messages: {
+      itemsPerPageOptionsLabel: "items per page",
+      currentPageItemsLabel: (start: number, end: number, total: number) =>
+        `${start}-${end} of ${total} items`,
+      pageCountLabel: (pageCount: number) => `of ${pageCount} pages`,
+      singlePageLabel: "1 page",
+      previousPageButtonLabel: "Previous page",
+      nextPageButtonLabel: "Next page",
+    },
+  },
+} satisfies Meta<typeof Pagination>;
 
 export default meta;
 
