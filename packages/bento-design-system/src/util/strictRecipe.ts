af94c780210e12aefff28baa886b94f8098200bd
Gabriele Petronella <gabriele@buildo.io>
2023-07-07 15:15:49 +0200

    Inline types that were removed from @vanilla-extract/recipes

--- packages/bento-design-system/src/util/strictRecipe.ts
+++ packages/bento-design-system/src/util/strictRecipe.ts
@@ -1,16 +1,30 @@
 import { recipe } from "@vanilla-extract/recipes";
-import {
-  PatternOptions,
-  VariantGroups,
-} from "@vanilla-extract/recipes/dist/declarations/src/types";
+import { ComplexStyleRule } from "@vanilla-extract/css";
 
-declare type BooleanMap<T> = T extends "true" | "false" ? boolean : T;
-
+type RecipeStyleRule = ComplexStyleRule | string;
+type VariantDefinitions = Record<string, RecipeStyleRule>;
+type BooleanMap<T> = T extends "true" | "false" ? boolean : T;
+type VariantGroups = Record<string, VariantDefinitions>;
 type VariantSelection<Variants extends VariantGroups> = {
-  [VariantGroup in keyof Variants]: BooleanMap<keyof Variants[VariantGroup]>;
+  [VariantGroup in keyof Variants]?: BooleanMap<keyof Variants[VariantGroup]>;
+};
+type RequiredVariantSelection<Variants extends VariantGroups> = {
+  [VariantGroup in keyof Variants]?: BooleanMap<keyof Variants[VariantGroup]>;
+};
+interface CompoundVariant<Variants extends VariantGroups> {
+  variants: VariantSelection<Variants>;
+  style: RecipeStyleRule;
+}
+type PatternOptions<Variants extends VariantGroups> = {
+  base?: RecipeStyleRule;
+  variants?: Variants;
+  defaultVariants?: VariantSelection<Variants>;
+  compoundVariants?: Array<CompoundVariant<Variants>>;
 };
 
-type RuntimeFn<Variants extends VariantGroups> = (options: VariantSelection<Variants>) => string;
+type RuntimeFn<Variants extends VariantGroups> = (
+  options: RequiredVariantSelection<Variants>
+) => string;
 
 /** A stricter version of 'recipe' in which all parameters are required */
 export function strictRecipe<Variants extends VariantGroups>(
