ac80ee5825edd3dfe8eddf4260dacab58bd24f51
Vincenzo Guerrisi <vincenzo@buildo.io>
2023-08-09 13:55:32 +0200

    Implement Interactive view

--- packages/configuration-builder/src/ColorsSection/ColorsSection.tsx
+++ packages/configuration-builder/src/ColorsSection/ColorsSection.tsx
@@ -2,6 +2,9 @@ import { useTranslation } from "react-i18next";
 import { ConfiguratorSection } from "../ConfiguratorSection/ConfiguratorSection";
 import { BrandColors } from "./BrandColors";
 import { ThemeConfig } from "../ThemeConfigurator/ThemeConfigurator";
+import { useState } from "react";
+import { match } from "ts-pattern";
+import { InteractiveColor } from "./InteractiveColor";
 
 type ColorsConfig = ThemeConfig["colors"];
 
@@ -11,24 +14,43 @@ type Props = {
 };
 
 export function ColorsSection(props: Props) {
+  const steps = [
+    "brand" as const,
+    "interactive" as const,
+    "neutral" as const,
+    "semantic" as const,
+    "dataVisualization" as const,
+  ];
   const { t } = useTranslation();
+
+  const [currentStep, setCurrentStep] = useState<(typeof steps)[0]>("brand");
 
   return (
     <ConfiguratorSection
       title={t("ColorsSection.title")}
-      steps={[
-        { label: t("ColorsSection.Step.brand") },
-        { label: t("ColorsSection.Step.interactive") },
-        { label: t("ColorsSection.Step.neutral") },
-        { label: t("ColorsSection.Step.semantic") },
-        { label: t("ColorsSection.Step.dataVisualization") },
-      ]}
-      currentStep={0}
+      steps={steps.map((step) => ({ label: t(`ColorsSection.Step.${step}`) }))}
+      currentStep={steps.indexOf(currentStep)}
     >
-      <BrandColors
-        value={props.value.brand}
-        onChange={(value) => props.onChange({ ...props.value, brand: value })}
-      />
+      {match(currentStep)
+        .with("brand", () => (
+          <BrandColors
+            value={props.value.brand}
+            onChange={(value) => props.onChange({ ...props.value, brand: value })}
+            onCancel={() => {}}
+            onNext={() => setCurrentStep("interactive")}
+          />
+        ))
+        .with("interactive", () => (
+          <InteractiveColor
+            value={props.value.interactive}
+            onChange={(value) => props.onChange({ ...props.value, interactive: value })}
+            brandColors={props.value.brand}
+            onBack={() => setCurrentStep("brand")}
+            onNext={() => setCurrentStep("neutral")}
+          />
+        ))
+        .with("neutral", "semantic", "dataVisualization", () => null)
+        .exhaustive()}
     </ConfiguratorSection>
   );
 }
