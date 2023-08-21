74ff26a812250d478206698fd8ce58371929d436
Vincenzo Guerrisi <vincenzo@buildo.io>
2023-08-21 16:38:05 +0200

    add copy palette color to clipboard

--- packages/configuration-builder/src/ColorEditor/Palette.tsx
+++ packages/configuration-builder/src/ColorEditor/Palette.tsx
@@ -1,5 +1,8 @@
 import { Box } from "@buildo/bento-design-system";
 import { LightnessInterpolation } from "./ColorEditor";
+import { useState } from "react";
+import { IconEyedropper } from "../Icons/IconEyedropper";
+import { HSLToHex, HexColor } from "../utils/colorUtils";
 
 type Props = {
   hue: number;
@@ -13,6 +16,39 @@ const interpolations: Record<LightnessInterpolation, number[]> = {
   EaseOut: [97, 86, 75, 64, 53, 42, 32, 25, 19, 14, 10],
   EaseInOut: [97, 93, 87, 79, 67, 54, 41, 28, 20, 14, 10],
 };
+
+export function PaletteColorBox(props: { color: HexColor }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(props.color);
+  }
+
+  return (
+    <Box
+      flexGrow={1}
+      padding={16}
+      borderRadius={16}
+      style={{
+        flexBasis: "100%",
+        backgroundColor: props.color,
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      onClick={copyToClipboard}
+      cursor="pointer"
+    >
+      {isHovered && (
+        <Box borderRadius={8} padding={8} background="backgroundDarkScrim">
+          <IconEyedropper size={24} color="primaryInverse" />
+        </Box>
+      )}
+    </Box>
+  );
+}
 
 export function Palette(props: Props) {
   const { hue, saturation, lightnessInterpolation } = props;
@@ -30,20 +66,8 @@ export function Palette(props: Props) {
     >
       <Box display="flex" gap={4} flexGrow={1}>
         {lightnesses.map((lightness) => {
-          const backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-
-          return (
-            <Box
-              flexGrow={1}
-              key={lightness}
-              padding={16}
-              borderRadius={16}
-              style={{
-                flexBasis: "100%",
-                backgroundColor,
-              }}
-            />
-          );
+          const color = HSLToHex({ h: hue, s: saturation, l: lightness });
+          return <PaletteColorBox color={color} key={lightness} />;
         })}
       </Box>
     </Box>
