dbe701ee92df20276ccc3a697346935439f54e54
Federico Ercoles <federico.ercoles@buildo.io>
2023-09-21 17:46:38 +0200

    Fix withBentoConfig utility

--- packages/bento-design-system/src/util/withBentoConfig.tsx
+++ packages/bento-design-system/src/util/withBentoConfig.tsx
@@ -1,7 +1,7 @@
 import { PartialBentoConfig } from "..";
 import { BentoConfigProvider } from "../BentoConfigContext";
 
-export function withBentoConfig<Props extends JSX.IntrinsicAttributes>(
+export function withBentoConfig<Props extends {}>(
   config: PartialBentoConfig,
   Component: (props: Props) => JSX.Element
 ): (props: Props) => JSX.Element {
