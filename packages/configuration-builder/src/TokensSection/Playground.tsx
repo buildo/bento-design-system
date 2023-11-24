1b25fba97be3b74463f1be44f48110ee15881bd2
Vincenzo Guerrisi <vincenzo@buildo.io>
2023-11-24 12:43:56 +0100

    Make Playgrounds inert

--- packages/configuration-builder/src/TokensSection/Playground.tsx
+++ packages/configuration-builder/src/TokensSection/Playground.tsx
@@ -5,16 +5,18 @@ export function Playground({ children }: { children: Children }) {
   const theme = useConfiguredTheme();
 
   return (
-    <Box
-      borderRadius={24}
-      padding={40}
-      background="backgroundSecondary"
-      display="flex"
-      flexDirection="column"
-      flexGrow={1}
-      overflow="hidden"
-    >
-      <BentoThemeProvider theme={theme}>{children}</BentoThemeProvider>
-    </Box>
+    <div ref={(node) => node && node.setAttribute("inert", "true")}>
+      <Box
+        borderRadius={24}
+        padding={40}
+        background="backgroundSecondary"
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        overflow="hidden"
+      >
+        <BentoThemeProvider theme={theme}>{children}</BentoThemeProvider>
+      </Box>
+    </div>
   );
 }
