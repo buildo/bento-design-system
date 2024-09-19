f640c0ce7bcd78bd240fbbfe40b1f8b00da3d9f9
Vincenzo Guerrisi <vincenzo@buildo.io>
2024-09-19 14:48:18 +0200

    rename controlled disclosure story

--- packages/bento-design-system/stories/Components/Disclosure.stories.tsx
+++ packages/bento-design-system/stories/Components/Disclosure.stories.tsx
@@ -44,7 +44,7 @@ export const InCard = {
   ],
 } satisfies Story;
 
-export const DelegateOpenControl = {
+export const Controlled = {
   decorators: [
     (Story) => {
       const [open, setOpen] = useState(true);
